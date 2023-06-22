91595b127cbb71dd703b34342d4b365ff3f8529a
Derecky Costa <dereckycosta@gmail.com>
2023-06-22 02:13:43 -0300

    feat: add tokens

--- src/lib/stitches/stitches-config.ts
+++ src/lib/stitches/stitches-config.ts
@@ -7,6 +7,8 @@ import {
   lineHeights,
   radii,
   space,
+  fonts,
+  media
 } from '../../themes'
 
 export const {
@@ -32,5 +34,8 @@ export const {
     lineHeights,
     radii,
     space,
+    fonts
   },
+
+  media
 })
