0194c92d641d7e9bebc4ef1b77bf71d84807c451
Alexandre <xnd-dev@gmail.com>
2023-07-04 22:13:38 -0300

    first commit

---
+++ src/components/slider/Carousel.styles.tsx
@@ -0,0 +1,19 @@
+import { styled } from '@/lib/stitches'
+
+export const SectionContainer = styled('section', {
+  display: 'flex',
+  boxSizing: 'border-box',
+  width: '90rem',
+  height: '40rem',
+  backgroundColor: '$neutral-0',
+})
+
+export const CarouselContainer = styled('div', {
+  width: '42rem',
+  height: '20.75rem',
+})
+
+export const CarouselContent = styled('div', {
+  width: '42rem',
+  height: '10,6875rem',
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/header/header'
+import { Carousel } from '@/components/slider'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Carousel />
     </div>
   )
 }
