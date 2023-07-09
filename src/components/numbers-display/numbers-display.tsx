9a511bc2ebf6f424e3feb9ef663d5b3d02cb7a4e
Alexandre <xnd-dev@gmail.com>
2023-07-09 17:59:15 -0300

    style: card container change color

--- src/components/numbers-display/numbers-display.tsx
+++ src/components/numbers-display/numbers-display.tsx
@@ -17,11 +17,11 @@ export function NumbersDisplay() {
           </S.SubtitleContainer>
         </S.TextsContainer>
         <S.CardsContainer>
-          <S.BlueContainer>
+          <S.VariantContainer>
             <Smiley size={'2rem'} />
             <S.StrongContainer> 6000 </S.StrongContainer>
             <S.InsideTextContainer> Pessoas impactadas </S.InsideTextContainer>
-          </S.BlueContainer>
+          </S.VariantContainer>
           <S.VariantContainer>
             <UsersThree size={'2rem'} />
             <S.StrongContainer>850</S.StrongContainer>
@@ -29,13 +29,13 @@ export function NumbersDisplay() {
               Famílias atendidas mensalmente
             </S.InsideTextContainer>
           </S.VariantContainer>
-          <S.BlueContainer>
+          <S.VariantContainer>
             <HandHeart size={'2rem'} />
             <S.StrongContainer>1500</S.StrongContainer>
             <S.InsideTextContainer>
               Pessoas ajudadas em abril de 2023
             </S.InsideTextContainer>
-          </S.BlueContainer>
+          </S.VariantContainer>
           <S.VariantContainer>
             <CalendarBlank size={'2rem'} />
             <S.StrongContainer>13</S.StrongContainer>
