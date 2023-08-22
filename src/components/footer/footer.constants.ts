98e48b6344563d59a0fdfbed4644ec4fe3b56068
Alexandre <xnd-dev@gmail.com>
2023-08-22 19:23:19 -0300

    first commit

--- src/components/footer/footer.constants.ts
+++ src/components/footer/footer.constants.ts
@@ -1,6 +1,6 @@
 export const links = {
   email: 'mailto:contato@ipas.com',
-  whatsapp: 'https://wa.me/5582996621031',
+  whatsapp: 'https://wa.me/5582991428156',
   linkedin: 'https://www.linkedin.com/company/institutoipas/',
   instagram: 'https://instagram.com',
   facebook: 'https://facebook.com',
