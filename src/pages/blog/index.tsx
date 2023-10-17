a81d0f21b5b8935a3e962ce67d6e9a83c38cd739
BrunoBatistaSoares <124104827+BrunoBatistaSoares@users.noreply.github.com>
2023-10-17 15:44:05 -0300

    Blog first commit

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/components/footer/footer";
+import { Header } from "@/components/header/header";
+
+export default function Blog() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
