b5cc989238e5690b11b39fc3c8e5e62448b57b0f
CCF-Indicium <claudia.fonseca@indicium.tech>
2024-03-19 17:25:29 -0300

    fix: Conserto da localização e tamanho das imagens flutuantes do site

--- src/styles/globals.styles.ts
+++ src/styles/globals.styles.ts
@@ -9,15 +9,22 @@ export const globalStyles = globalCss({
     scrollMarginTop: '5.5rem',
     scrollSnapMarginTop: '5.5rem',
     scrollBehavior: 'smooth',
+    position:'relative'
   },
 
   body: {
     backgroundColor: '$neutral-0',
     color: '$black',
     position:'relative',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'column',
     margin: 0,
     padding: 0,
     '-webkit-font-smoothing': 'antialiased',
+    overflowX:'hidden',
+    textAligh:'center',
   },
 
   'body, input, textarea, button': {
