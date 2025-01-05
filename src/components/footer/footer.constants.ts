d3ac9cd4736e8a109ef60179a323515dc6454d08
Alexandre Barbosa <xnd.dev@gmail.com>
2025-01-05 19:41:27 -0300

    fix: ipas number

--- src/components/footer/footer.constants.ts
+++ src/components/footer/footer.constants.ts
@@ -1,6 +1,6 @@
 export const links = {
   email: 'mailto:contato@ipas.com',
-  whatsapp: 'https://wa.me/5582991428156',
+  whatsapp: 'https://wa.me/5582996621031',
   linkedin: 'https://www.linkedin.com/company/institutoipas/',
   instagram: 'https://www.instagram.com/projetoipas/',
   facebook: 'https://facebook.com',
